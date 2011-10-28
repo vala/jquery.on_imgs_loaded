5af8487b1941cdf689b8c3cfe3471c9161254f9a
vala <gonjo@free.fr>
2011-10-28 23:03:36 +0200

    Review and add readme

--- jquery.on_imgs_loaded.js
+++ jquery.on_imgs_loaded.js
@@ -3,25 +3,22 @@
   $.fn.onImgsLoaded = function(cb) {
   	
 		var images_len = this.length,
-				images_loaded = 0;
+				images_loaded = 0,
+				images = this,
+				cb_args = Array.prototype.slice.call(arguments, 1);
 		
+		// If cb is no function, then return
 		if(!$.isFunction(cb))
 			return this;
 		
-		return this.each(function() {
-			var $this = $(this);
-			
-			// Si l'élément actuel n'est pas 
-			if(typeof this.src === 'undefined')
-				return;
-				
+		return this.each(function() {			
 			image = new Image();
 			$(image)
 				.load(function() {
 					if(++images_loaded == images_len)
-						cb();
+						cb.call(images, cb_args);
 				});
-			image.src = $(this).attr('src');	
+			image.src = this.src;	
 		});				
   };
 })( jQuery );
